97de683dd0364c087e1556518720fe3cec47cfbe
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:18:33 +0200

    kitakami: Build USB HIDL HAL service
    Change-Id: I993d76c70f50ae6f8f6c3f468d9a363fad6711d5

--- device-common.mk
+++ device-common.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Wlan
 PRODUCT_PACKAGES += macaddrsetup
 
